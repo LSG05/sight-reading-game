664c9f0e09784ac7e6ac8713d43e1b666656c0a3
Maxynne Castro <139049713+maxynnecastro@users.noreply.github.com>
2026-04-02 13:34:42 +0800

    add masterclock, audioservice, and animationtimer to initial sequence

--- src/main/java/com/sightreading/GameController.java
+++ src/main/java/com/sightreading/GameController.java
@@ -1,15 +1,16 @@
 package com.sightreading;
 
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import javafx.animation.AnimationTimer;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.shape.Rectangle;
-
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.ResourceBundle;
 
 public class GameController implements Initializable {
 
@@ -22,6 +23,29 @@ public class GameController implements Initializable {
     // The song to load. In a later phase this will be passed in
     // from a song selection screen. For now it is hardcoded.
     private static final String SONG_FOLDER = "songs/twinkle";
+    private static String SONG_PATH_STRING = "/com/sightreading/songs/twinkle/audio.wav"; // hard code temporary
+
+    // Declare variables from masterclock and audioservice
+    private MasterClock masterClock = new MasterClock();
+    private AudioService audioService = new AudioService(SONG_PATH_STRING);
+
+    // Flattened note list for reading
+    private List<NoteData> songNoteList;
+
+    //
+    private int noteIndex = 0;
+
+    // Declare animationTimer for frame by frame activities
+    private AnimationTimer animationTimer = new AnimationTimer() {
+        @Override
+        public void handle(long now){
+            if (!masterClock.isRunning()) return;
+            long elapsedMs = masterClock.getElapsedMs();
+            double computedX = computeHitBox(elapsedMs);
+            setHitBoxX(computedX);
+        }
+    };
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -41,6 +65,25 @@ public class GameController implements Initializable {
         hitBox.setTranslateX(0);
 
         System.out.println("Phase 1 complete. Window ready.");
+
+        // 5. Create noteslist for easy access of notes from json file
+        songNoteList = new ArrayList<>();
+        for(LineData line: songData.lines){
+            for(NoteData note: line.notes){
+                if(note != null){
+                    songNoteList.add(note);
+                }
+            }
+        }
+
+        // 6. Load Audio
+        audioService.loadSong();
+
+        // 7. Run sequence
+        audioService.playSong();
+        masterClock.start();
+        animationTimer.start();
+
     }
 
     private void preloadAllImages() {
@@ -75,4 +118,20 @@ public class GameController implements Initializable {
     public void setHitBoxX(double x) {
         hitBox.setTranslateX(x);
     }
+
+    public double computeHitBox(long elapsedMs){
+        if(songNoteList.size() == 0){return 0;}
+        int index = this.noteIndex;
+        long prevNoteTime = songNoteList.get(index).targetTimeMs;
+        long nextNoteTime = songNoteList.get(index + 1).targetTimeMs;
+        long noteXPrevDist = songNoteList.get(index).pixelX;
+        long noteXNextDist = songNoteList.get(index + 1).pixelX;
+        double hitBoxPosition = noteXPrevDist + ((double)(elapsedMs-prevNoteTime)/(nextNoteTime-prevNoteTime))*(noteXNextDist - noteXPrevDist);
+        if (elapsedMs >= nextNoteTime && noteIndex < songNoteList.size() - 1) {
+            noteIndex++;
+        }
+        return hitBoxPosition;
+    }
 }
+
+// stop methods, make sure box is on top, implement separate thread for audioservice, edge cases and bounds + address delay, switch images
