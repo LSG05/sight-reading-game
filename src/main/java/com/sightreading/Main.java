5caa7dd8900b74d0ff52c3c5fe12d651b5a11bc6
Myles Dominique <mylesdominiquep@gmail.com>
2026-04-03 10:04:34 +0800

    added a home page and scene switching in Main.java

--- src/main/java/com/sightreading/Main.java
+++ src/main/java/com/sightreading/Main.java
@@ -2,23 +2,33 @@ package com.sightreading;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Main extends Application {
+
+    private static Scene scene;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(
-            getClass().getResource("/com/sightreading/game.fxml")
-        );
-
-        Scene scene = new Scene(loader.load(), 1200, 900);
+        scene = new Scene(loadFXML("home"), 1200, 900); 
 
         primaryStage.setTitle("Sight Reading Game");
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
         primaryStage.show();
+    }
+
+    public static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/com/sightreading/" + fxml + ".fxml"));
+        return fxmlLoader.load();
     }
 
     public static void main(String[] args) {
